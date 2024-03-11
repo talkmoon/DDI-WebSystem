f1d5bb90fdfbdfbe82346febcacb7e9e04d2922c
ming <umrrmu666@gmail.com>
2024-03-11 21:40:09 +0800

    界面优化

--- src/components/DualCIntro.vue
+++ src/components/DualCIntro.vue
@@ -1,72 +1,63 @@
 <template>
 
-  <section class="p-5">
-    <div class="container">
-      <div class="row align-items-center justify-content-between">
-        <h1>MSResG模型介绍</h1>
-        <span>--------------------------------------------------------------------------------------------------------------</span>
-        <h2></h2>
-        <h2></h2>
-        <h2>药物联合治疗：</h2>
-        <p class="lead">随着批准的药物数量快速增加，药物联合治疗逐渐成为治疗癌症、心脑血管疾病等多种疾病的有效方法。我们乐见于有益结果，让药物组合产生“1+1>2”的治疗增效作用，但实际上DDI的发生往往也会导致一些意想不到的不良事件。所以对DDI的研究很有必要。
-        </p>
-      </div>
-    </div>
-  </section>
-
-  <section class="p-5">
-    <div class="container">
-      <div class="row align-items-center justify-content-between">
-        <h1>药物-药物相互作用常见的研究领域有哪些？</h1>
-        <span>--------------------------------------------------------------------------------------------------------------</span>
-        <h2></h2>
-        <h2></h2>
-        <h2>药物联合治疗：</h2>
-        <p class="lead">随着批准的药物数量快速增加，药物联合治疗逐渐成为治疗癌症、心脑血管疾病等多种疾病的有效方法。我们乐见于有益结果，让药物组合产生“1+1>2”的治疗增效作用，但实际上DDI的发生往往也会导致一些意想不到的不良事件。所以对DDI的研究很有必要。
-        </p>
-      </div>
-    </div>
-  </section>
+  <div id="building5"></div>
 
   <section class="p-5 bg-body-secondary text-dark">
     <div class="container">
       <div class="row align-items-center justify-content-between">
-        <div class="col-md">
-          <!-- -md让其缩小上下排列 -->
-          <img src="../img/DualCpic.jpg" alt="1" class="img-fluid">
-          <!-- 加上img-fluid 不会超出div的范围 -->
-        </div>
-        <div class="col-md p-5">
-          <h2>DrugBank数据库</h2>
-          <p class="lead">DrugBank数据库是药品信息领域的权威数据库，该数据库以药物为中心，提供丰富的生物信息学和化学信息学资源，如化学结构、靶标、副作用、临床应用和不良反应等，目前，DrugBank数据库已广泛应用于药物设计和开发、药物-药物相互作用预测等领域。
-          </p>
-          <router-link to="/DualCUse">
-            <a class="btn btn-light">查看更多DrugBank数据库信息</a>
-          </router-link>
-
-        </div>
+        <h2>DualC模型引入</h2>
+        <span>--------------------------------------------------------------------------------------------------------------</span>
+        <h5 class="lead">
+          MSResG模型借助药物的拓扑特征进行药物-药物相互作用预测。实际上除了药物拓扑信息外，还有很多其他的药物信息值得考虑，如药物结构信息，药物副作用信息、药物靶标信息等，
+          考虑到药物-药物相互作用预测领域中被广泛应用的“相似的药物具有相似的药物-药物相互作用关系”这一假设，所以利用药物结构信息补充药物拓扑信息，
+          富药物的潜在特征向量。由经典的图卷积网络公式可知，使用L层图卷积网络提取特征之后，结点容易出现信息丢失或信息过度平滑的问题，信息丢失是指随着层数的增加，
+          部分结点的特征可能会受到周围结点特征的覆盖或稀释，导致部分重要信息丢失；信息过度平滑是指随着层数的增加，不同结点的特征表示可能会逐渐趋同，使得结点之间的差异性减弱，
+          影响了模型的表达能力。所以引入层注意力机制，使用具有层注意力机制的图卷积网络对所提取到的各层药物特征综合考虑，基于此，
+          本系统中该方法介绍一种基于药物拓扑信息和结构信息的双特征提取的药物-药物相互作用预测方法，名为DualC。
+        </h5>
       </div>
     </div>
   </section>
 
-  <!--  <section class="p-5  ">-->
-  <!--    <div class="container">-->
-  <!--      <div class="row align-items-center justify-content-between">-->
 
-  <!--        <div class="col-md">-->
-  <!--          <h2>PubChem数据库</h2>-->
-  <!--          <p class="lead">PubChem数据库是一个公共化学数据库，该数据库关注全球范围内的化合物信息，介绍了化学结构、标识符、化学和物理性质、生物活性、毒性数据等信息，目前，DrugBank数据库在药物设计和开发、毒性评估与预测等领域都有广泛应用。-->
-  <!--          </p>-->
-  <!--          <a href="https://pubchem.ncbi.nlm.nih.gov/" class="btn btn-light">查看更多PubChem数据库信息</a>-->
-  <!--        </div>-->
-  <!--        <div class="col-md p-5">-->
-  <!--          &lt;!&ndash; -md让其缩小上下排列 &ndash;&gt;-->
-  <!--          <img src="../img/PubChem.png" alt="1" class="img-fluid">-->
-  <!--          &lt;!&ndash; 加上img-fluid 不会超出div的范围 &ndash;&gt;-->
-  <!--        </div>-->
-  <!--      </div>-->
-  <!--    </div>-->
-  <!--  </section>-->
+  <section class="p-5">
+    <div class="container">
+      <div class="row align-items-center justify-content-between">
+        <div class="col-md">
+          <img src="../img/DualCpic.jpg" alt="1" class="img-fluid">
+          <p></p>
+          <p><h3>---------------DualC模型框架图----------------</h3></p>
+        </div>
+
+        <div class="col-md p-5">
+          <h2>DualC模型介绍</h2>
+          <el-timeline style="max-width: 600px">
+            <el-timeline-item timestamp="" placement="top">
+              <el-card>
+                <h5 class="lead">
+                              DualC是一种基于双特征提取的药物-药物相互作用预测方法，利用具有层注意力机制的GCN和1D-CNN分别提取药物的拓扑信息和结构信息。
+                              DualC的整体框架图如图4-1所示：首先，从数据库中获得药物-药物相互作用网络，并借助药物靶标特征计算药物靶标相似性，然后将其结合构建为异构网络。
+                              其次借助药物化学子结构特征计算药物化学子结构相似性矩阵。然后，利用具有层注意机制的GCN从药物异构网络中提取药物的拓扑特征，
+                              并基于“相似的药物具有相似的药物-药物相互作用关系”这一假设，利用1D-CNN从药物的化学子结构相似性矩阵中提取药物的结构信息。
+                              最后，将药物的拓扑信息特征和结构信息特征进行拼接，使用sigmoid函数实现药物-药物相互作用预测。
+                </h5>
+
+              </el-card>
+            </el-timeline-item>
+            <el-timeline-item timestamp="" placement="top">
+              <el-card>
+                <router-link to="/MSResGUse">
+                  <a class="btn btn-secondary" >点击此处使用MSResG模型进行训练与预测</a>
+                </router-link>
+              </el-card>
+            </el-timeline-item>
+            <el-timeline-item timestamp=" " placement="top">
+            </el-timeline-item>
+          </el-timeline>
+        </div>
+      </div>
+    </div>
+  </section>
 
 
 
@@ -86,4 +77,21 @@ import { ref } from 'vue'
 const currentDate = ref(new Date())
 </script>
 
+<style type="text/css">
+h5 {
+  text-align: justify;
+}
+</style>
+<style>
+#building5{
+  background:url("src/img/beijing6.png");
+  width:100%;
+  height:100%;
+  position:fixed;
+  background-size:100% 100%;
+  z-index: -1;
+  top: 0;
+  left: 0;
+}
+</style>
 
--- src/components/MSResGIntro.vue
+++ src/components/MSResGIntro.vue
@@ -1,6 +1,6 @@
 <template>
 
-
+  <div id="building4"></div>
 
   <section class="p-5 ">
     <div class="container">
@@ -13,12 +13,44 @@
           <!-- 加上img-fluid 不会超出div的范围 -->
         </div>
         <div class="col-md p-5">
-          <h1>MSResG模型：</h1>
-          <p class="lead">DrugBank数据库是药品信息领域的权威数据库，该数据库以药物为中心，提供丰富的生物信息学和化学信息学资源，如化学结构、靶标、副作用、临床应用和不良反应等，目前，DrugBank数据库已广泛应用于药物设计和开发、药物-药物相互作用预测等领域。
-          </p>
-          <router-link to="/MSResGUse">
-            <a class="btn btn-light">点击</a>
-          </router-link>
+          <h2>MSResG模型：</h2>
+            <el-timeline style="max-width: 600px">
+              <el-timeline-item timestamp="MSResG模型的引入背景" placement="top">
+                <el-card>
+                  <h5 class="lead">基于深度学习的药物-药物相互作用预测通常使用经典的图卷积网络从药物-药物相互作用网络中提取药物的潜在拓扑特征。
+                    现如今，经典的图卷积网络在现实领域的各项研究中都发展迅速，但是它容易出现梯度消失的问题，所以经典的图卷积网络通常局限于比较浅的深度学习模型，
+                    在实际研究中，常见的图卷积网络层数大多为1至3层，所以引入残差图卷积网络，旨在提高信息的传递效率和准确性、提高模型的泛化能力。
+                    又因为药物-药物相互作用网络中只包含药物之间的相互作用信息，所以本章还引入了基于多种药物特征的药物相似性信息，
+                    包括药物靶标相似性、药物酶相似性、药物子结构相似性和药物通路相似性，用以补充药物拓扑信息的丰富性。
+                    基于此，该系统提供一种考虑多源药物特征和残差图卷积网络的药物-药物相互作用预测模型，名为MSResG。
+                  </h5>
+
+                </el-card>
+              </el-timeline-item>
+              <el-timeline-item timestamp="MSResG模型的介绍" placement="top">
+                <el-card>
+                  <h5 class="lead">MSResG模型的实现步骤如下：
+                    首先，该模型从药物相关数据库中获取药物化学子结构、药物靶标、药物通路和药物酶四种药物特征，并基于此将各个药物表示为二元向量进而计算药物的Jaccard相似性，
+                    由此可以获得药物化学子结构相似性、药物靶标相似性、药物通路相似性和药物酶相似性，为了综合考虑不同的药物特征，
+                    求取平均值的方式来进行相似性融合得到药物综合相似性网络。然后，为了补充药物-药物相互作用网络的信息，将药物综合相似性网络与药物-药物相互作用网络相结合，
+                    构建为异构网络。最后，使用图自编码器对异构网络进行编码和解码以实现药物-药物相互作用预测，编码过程即是使用残差图卷积网络从异构网络中学习药物的潜在拓扑特征向量，
+                    这些特征向量既包含药物的相似性信息，也包含药物的相互作用信息，解码过程则是重建网络以实现对潜在药物-药物相互作用的预测。
+                  </h5>
+                </el-card>
+              </el-timeline-item>
+              <el-timeline-item timestamp=" " placement="top">
+                <el-timeline-item timestamp="使用" placement="top">
+                  <el-card>
+                    <router-link to="/MSResGUse">
+                      <a class="btn btn-secondary" >点击此处使用MSResG模型进行训练与预测</a>
+                    </router-link>
+                  </el-card>
+                </el-timeline-item>
+
+            </el-timeline-item>
+            </el-timeline>
+
+
 
         </div>
       </div>
@@ -44,4 +76,23 @@ import { ref } from 'vue'
 const currentDate = ref(new Date())
 </script>
 
+<style type="text/css">
+h5 {
+  text-align: justify;
+}
+</style>
 
+<style>
+
+#building4{
+  background:url("src/img/beijing5.png");
+  width:100%;
+  height:100%;
+  position:fixed;
+  background-size:100% 100%;
+  z-index: -1;
+  top: 0;
+  left: 0;
+}
+
+</style>
